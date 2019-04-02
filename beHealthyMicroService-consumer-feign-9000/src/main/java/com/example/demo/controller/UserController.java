50d4004cce52e5eb0c12d2ef3b7d4417fc559540
Joshioner <550942828@qq.com>
2019-04-02 11:17:19 +0800

    封装统一返回格式 + 异常处理

--- beHealthyMicroService-consumer-feign-9000/src/main/java/com/example/demo/controller/UserController.java
+++ beHealthyMicroService-consumer-feign-9000/src/main/java/com/example/demo/controller/UserController.java
@@ -2,9 +2,10 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
+import com.example.demo.util.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 @RequestMapping("/user")
@@ -14,23 +15,31 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/login")
-    public User login(@RequestBody User user){
-        return userService.login(user);
+    public String login(@RequestBody User user){
+         user = userService.login(user);
+         if (user == null)
+             return JsonResult.failed(-1,"找不到该用户");
+        return JsonResult.success(user);
     }
 
     @PostMapping("/register")
-    public Integer register(@RequestBody User user){
-        return userService.register(user);
+    public String register(@RequestBody User user){
+          Integer num = userService.register(user);
+          return JsonResult.success(num);
     }
 
     @GetMapping("/findById/{id}")
-    public User findById(@PathVariable("id") Integer id){
-        return userService.findById(id);
+    public String findById(@PathVariable("id") Integer id){
+         User user =  userService.findById(id);
+         return JsonResult.success(user);
     }
 
     @PostMapping("/updateUser")
-    public Integer updateUser(@RequestBody User user){
-        return userService.updateUser(user);
+    public String updateUser(@RequestBody User user){
+        Integer num =  userService.updateUser(user);
+        if (num <= 0)
+            return JsonResult.failed(1,"更新失败");
+        return JsonResult.success(num);
     }
 
 }
---
+++ beHealthyMicroService-consumer-feign-9000/src/main/java/com/example/demo/util/JsonResult.java
@@ -0,0 +1,44 @@
+package com.example.demo.util;
+
+
+
+import com.alibaba.fastjson.JSON;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+@Data
+public class JsonResult implements Serializable{
+
+    private int code;   //返回码 非0即失败
+    private String msg; //消息提示
+    private Object data; //返回的数据
+
+    public JsonResult(){};
+
+    public JsonResult(int code, String msg, Object data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public static String success() {
+        return success(null);
+    }
+    public static String success(Object data) {
+        return JSON.toJSONString(new JsonResult(0, "解析成功", data));
+    }
+
+    public static String failed() {
+        return failed("解析失败");
+    }
+    public static String failed(String msg) {
+        return failed(-1, msg);
+    }
+    public static String failed(int code, String msg) {
+        return JSON.toJSONString(new JsonResult(code, msg, null));
+    }
+
+}
+
